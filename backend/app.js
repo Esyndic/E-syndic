f32f6c83e1f8101640aac1b802fe835ce25de5af
djobbihamed <djobbi.hamed@gmail.com>
2023-08-28 18:16:30 +0200

    done

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,17 @@
 const express = require('express');
 
-//Create an Express App
 const app = express();
 
-//Require application Route modules
 const postsRoute = require('./routes/syndic');
-const usersRoute = require('./routes/users');
+const tenantsRoute = require('./routes/tenants');
 
 
-//Middleware to parse incoming requests with JSON and urlencoded payloads
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-//Add Routes to the middleware handling path, specifying the respective URL path
-app.use('/api/posts', postsRoute);
-app.use('/api/users', usersRoute);
 
-module.exports = app; // export the express app.
+app.use('/api/posts', postsRoute);
+app.use('/api/tenants', tenantsRoute);
+
+module.exports = app; 
---
+++ backend/controllers/tenants.js
@@ -0,0 +1,47 @@
+const tenants = require("../database/models/tenants");
+
+module.exports = {
+
+  getAllTenants: function (req, res) {
+    tenants.getAll(function (err, results) {
+      if (err) res.status(500).send(err);
+      else res.json(results);
+    });
+  },
+
+  addTenants: function (req, res) {
+    tenants.add(function (err, results) {
+      if (err) res.status(500).send(err);
+      else res.json(results);
+    }, req.body);
+  },
+
+  
+  getOneTenants: function (req, res) {
+    tenants.getOne(function (err, results) {
+      if (err) res.status(500).send(err);
+      else res.json(results);
+    }, req.params.id);
+  },
+
+  
+  deleteTenants: function (req, res) {
+    tenants.delete(function (err, results) {
+      if (err) res.status(500).send(err);
+      else res.json(results);
+    }, req.params.id);
+  },
+
+
+
+  updateTenants: function (req, res) {
+    tenants.update(
+      function (err, results) {
+        if (err) res.status(500).send(err);
+        else res.json(results);
+      },
+      req.body,
+      req.params.id
+    );
+  },
+};

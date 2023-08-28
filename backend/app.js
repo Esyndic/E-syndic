6c66b5808d5bd643f25d8c2bff7fa807b5d99a7d
slimani-amine <slimanimedamine24@gmail.com>
2023-08-28 13:38:10 +0100

    first

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 //Require application Route modules
 const postsRoute = require('./routes/syndic');
 const usersRoute = require('./routes/users');
-const commentsRoute = require('./routes/comments');
+
 
 //Middleware to parse incoming requests with JSON and urlencoded payloads
 app.use(express.json());
@@ -15,6 +15,5 @@ app.use(express.urlencoded({extended: true}));
 //Add Routes to the middleware handling path, specifying the respective URL path
 app.use('/api/posts', postsRoute);
 app.use('/api/users', usersRoute);
-app.use('/api/comments', commentsRoute);
 
 module.exports = app; // export the express app.
--- backend/database/index.js
+++ backend/database/index.js
@@ -4,7 +4,12 @@ const mysql = require('mysql2');
 const config = require('config');
 
 // Create a database connection and export it from this file.
-const connection = mysql.createConnection(config);
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: '',
+  password: "A.m.i.n.e1"
+ });
 connection.connect((err)=>{
   if (err) {
     console.log(err);

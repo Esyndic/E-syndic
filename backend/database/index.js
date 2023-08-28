1617bb09c309906de8d3e10f31d2aadc82adab9d
slimani-amine <slimanimedamine24@gmail.com>
2023-08-28 17:30:00 +0100

    dd

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2');
 
-
+const config={
+  
+}
 // Create a database connection and export it from this file.
 const connection = mysql.createConnection(config);
 connection.connect((err)=>{

36144e79e67a0cc61b8caa6bea37074c5484a22d
slimani-amine <slimanimedamine24@gmail.com>
2023-08-28 18:52:44 +0100

    did

--- frontend/vite-project/components/NavBar.jsx
+++ frontend/vite-project/components/NavBar.jsx
@@ -5,19 +5,15 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 
-const pages = ["Login", "LoginAsSyndic"];
 const settings = ["Profile", "Logout"];
 
-function NavBar() {
+function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -40,68 +36,23 @@ function NavBar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Link to="/">
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              E-SYNDIC
-            </Typography>
-          </Link>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Link to="/login">
-                  <Typography textAlign="center">{page}</Typography>
-                </Link>
-              </MenuItem>
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Link to="/loginAsSyndic">
-                  <Typography textAlign="center">{page}</Typography>
-                </Link>
-              </MenuItem>
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <Link to="/">E-Syndic</Link>
+          </Typography>
+
           <Typography
             variant="h5"
             noWrap
@@ -118,26 +69,19 @@ function NavBar() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            E-SYNDIC
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
 
           <Box sx={{ flexGrow: 0 }}>
+            
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
+
             </Tooltip>
+
             <Menu
               sx={{ mt: "45px" }}
               id="menu-appbar"
@@ -154,11 +98,12 @@ function NavBar() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Typography textAlign="center"><Link to="/login">Profile</Link></Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Typography textAlign="center"><Link to="/">Logout</Link></Typography>
+              </MenuItem>
             </Menu>
           </Box>
         </Toolbar>
@@ -166,4 +111,4 @@ function NavBar() {
     </AppBar>
   );
 }
-export default NavBar;
+export default ResponsiveAppBar;
--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import LoginAsTenant from "../components/LoginAsTenant";
 import Login from "../components/Login";
+import Home from "../components/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
+
       <Routes>
-        <Route path="/" elemenet={<Home />} />
-        <Route path="/loginAsSyndic" elemenet={<LoginAsTenant />} />
-        <Route path="/login" elemenet={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/loginAsSyndic" element={<LoginAsTenant />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );

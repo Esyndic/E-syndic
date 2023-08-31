9583d5b12d8b32d39d2d2e80d519506e865b9260
slimani-amine <slimanimedamine24@gmail.com>
2023-08-31 14:27:19 +0100

    done

--- frontend/vite-project/Scomp/feedbacks.jsx
+++ frontend/vite-project/Scomp/feedbacks.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import SNavbar from "./SNavBar.jsx";
-import Cards from "./Cards.jsx";
+import axios from "axios";
+import "../Tcomp/facture.css";
 function Feedbacks(props) {
+  const [claims, setClaims] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/claims/getAll")
+      .then((response) => {
+        console.log(response.data);
+        setClaims(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div>
       <div>
         <SNavbar />
-
         <section class="section-2">
           <div class="sheet">
             <p class="parag">
-
-            <br/>
+              <br />
               Hello! Welcome to our platform's interface. This is your space to
               effortlessly submit claims, provide valuable feedback, and
               conveniently access your invoices. If you encounter any challenges
@@ -20,12 +31,35 @@ function Feedbacks(props) {
               support team is available round the clock, 7 days a week, to
               assist you. Your satisfaction is our priority, and we're here to
               serve you anytime, day or night.
-              <br/>
-              <br/>
-            
-              <br/>
-              
+              <br />
+              <br />
             </p>
+            <br />
+            <div id="fact">
+              {claims.map((claim, i) => {
+                return (
+                  <div class="parent">
+                    <div class="fcard">
+                      <div class="content-box">
+                        <span class="card-title">claim</span>
+                        <ol className="claim-list">
+                          <li key={claim.id}>
+                            <p></p>
+                            <p className="Tenant">id claim: {claim.idclaims}</p>
+                            <p>name : {claim.name}</p>
+                            <p>num : {claim.num}</p>
+                            <p>email: {claim.email}</p>
+                          </li>
+                        </ol>
+                      </div>
+                      <div class="date-box">
+                        <span class="date">{i}</span>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </section>
 

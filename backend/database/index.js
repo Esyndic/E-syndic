593395846b5da44cbaea10b9d0669d0f4d08a3d4
mohamed-trabels <mohamed930trabelsi930@gmail.com>
2023-08-31 17:09:51 +0100

    done

--- backend/database/index.js
+++ backend/database/index.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const config = {
   host: "localhost",
   user: "root",
-  password: "A.m.i.n.e1",
+  password: "root",
   database: "e-syndic",
 }
 // Create a database connection and export it from this file.
--- frontend/vite-project/Scomp/AddPers.jsx
+++ frontend/vite-project/Scomp/AddPers.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './AddPers.css';
-import SNavBar from "../Scomp/SNavBar.jsx"
-import axios from 'axios';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./AddPers.css";
+import SNavBar from "../Scomp/SNavBar.jsx";
+import axios from "axios";
 
 function AddPers() {
   const [name, setName] = useState("");
@@ -11,52 +10,109 @@ function AddPers() {
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("");
   const [image, setImage] = useState("");
-   
-  
-  const obj={
-    name:name,
-    num:num,
-    email:email,
-    role:role,
-    image:image,
-    syndic_idsyndic:1
-  }
 
- 
+  const obj = {
+    name: name,
+    num: num,
+    email: email,
+    role: role,
+    image: image,
+    syndic_idsyndic: 1,
+  };
+
   const addPersonnel = () => {
     axios
       .post(`http://localhost:3000/api/personnel/add`, obj)
-      .then((result) => {console.log(result)})
-      .catch((error) =>{ console.log(error)});
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
-
- 
 
   return (
     <div>
-<SNavBar/>
+      <SNavBar />
 
       <div className="container">
-    
         <div className="heading">Add Personnel</div>
         <form action="" className="form">
-          <input required className="input" type="text" 
-          name="name" id="name" placeholder="Name" onChange={(e)=>{setName(e.target.value)}} />
-          <input required className="input" type="text" 
-          name="num" id="num" placeholder="Number" onChange={(e)=>{setNum(e.target.value)}}/>
-          <input required className="input" type="text" 
-          name="email" id="email" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}}/>
-          <input required className="input" type="text" 
-          name="role" id="role" placeholder="Role"onChange={(e)=>{setRole(e.target.value)}} />
-          <input required className="input" type="text" 
-          name="image" id="image" placeholder="Image URL" onChange={(e)=>{setImage(e.target.value)}}/>
-
-        <Link to="/personnels"><input className="addbutton" 
-                type="submit" 
-                value="Add Personnel" 
-                  onClick={addPersonnel}
-                />  </Link>
-          
+          <input
+            required
+            className="input"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <input
+            required
+            className="input"
+            type="text"
+            name="num"
+            id="num"
+            placeholder="Number"
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          />
+          <input
+            required
+            className="input"
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            required
+            className="input"
+            type="text"
+            name="role"
+            id="role"
+            placeholder="Role"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          />
+          <input
+            required
+            className="input"
+            type="text"
+            name="image"
+            id="image"
+            placeholder="Image URL"
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+          />
+          <input
+            required
+            className="input"
+            type="file"
+            name="image"
+            id="image"
+            placeholder="Image URL"
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+          />
+          <button>click me </button>
+          <Link to="/personnels">
+            <input
+              className="addbutton"
+              type="submit"
+              value="Add Personnel"
+              onClick={addPersonnel}
+            />{" "}
+          </Link>
         </form>
       </div>
     </div>
